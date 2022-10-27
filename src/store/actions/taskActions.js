ba6fc0e000c42b5f605b9d83adda91cee3a0e4a9
SantiagoRensonnet <63661270+SantiagoRensonnet@users.noreply.github.com>
2022-10-27 18:23:55 -0300

    Create Redux Actions and Reducers

---
+++ src/store/actions/taskActions.js
@@ -0,0 +1,28 @@
+import { TASKS_REQUEST, TASKS_SUCCESS, TASKS_FAILURE } from "../types";
+
+const { REACT_APP_API_ENDPOINT: API_ENDPOINT } = process.env;
+
+export const tasksRequest = () => ({
+  type: TASKS_REQUEST,
+});
+export const tasksSuccess = (data) => ({
+  type: TASKS_SUCCESS,
+  payload: data,
+});
+export const tasksFailure = (error) => ({
+  type: TASKS_FAILURE,
+  payload: error,
+});
+
+export const getTasks = (path) => (dispatch) => {
+  dispatch(tasksRequest());
+  fetch(`${API_ENDPOINT}/tasks/${path}`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + sessionStorage.getItem("token"),
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => dispatch(tasksSuccess(data.result)))
+    .catch((error) => dispatch(tasksFailure(error)));
+};
